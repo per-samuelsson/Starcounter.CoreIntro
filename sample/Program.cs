ef5441e4f30d4b8eedcb54a3f5e1652f09af8caf
Per Samuelsson <per@starcounter.com>
2016-06-15 10:00:22 +0200

    Added minimal sample project, referencing our core surface area Starcounter.dll.

---
+++ sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Starcounter;
+
+namespace Sample {
+    public class Program {
+        public static void Main(string[] args) {
+            Db.Transact(() => {
+				Console.WriteLine("Sample transaction!");
+			});
+        }
+    }
+}
