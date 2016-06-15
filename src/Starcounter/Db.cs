62cdc0e94a4e37e581dcb1da38c7943713e006f1
Per Samuelsson <per@starcounter.com>
2016-06-15 09:12:55 +0200

    Added Starcounter project, including initial Db class stub.

---
+++ src/Starcounter/Db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Starcounter {
+
+    public partial class Db {
+        
+        public static void Transact(Action a) {
+            Console.WriteLine("Enter transaction");
+            a();
+            Console.WriteLine("Exit transaction");
+        }
+    }
+}
